3030f9e6d6abbcb43286cb445c85847017fcdd3c
9731130 <ahmadrezadl@aut.ac.ir>
2019-06-02 11:50:47 +0430

    Loading Added!

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -5,12 +5,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
+import java.io.IOException;
 
 public class MainFrame extends JFrame{
     Image img;
     public MainFrame(String title) throws HeadlessException {
         super(title);
-        System.out.println("Main Frame Start Adding...");
+        JFrame loadingFrame = new JFrame("Loading...");
+        JLabel Logo = new JLabel();
+        Image jpotifyLogo = null;
+        try {
+            jpotifyLogo = ImageIO.read(getClass().getResource("icons\\logo.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Logo.setIcon(new ImageIcon(jpotifyLogo));
+        loadingFrame.add(Logo);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        loadingFrame.setSize(520,210);
+        loadingFrame.setBackground(Color.WHITE);
+        loadingFrame.setResizable(false);
+        loadingFrame.setLocationRelativeTo(null);
+        loadingFrame.setUndecorated(true);
+        loadingFrame.setVisible(true);
+        System.out.println ("Main Frame Start Adding...");
         this.setLayout(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(100,100);
@@ -27,6 +46,7 @@ public class MainFrame extends JFrame{
         add(centerMenu , BorderLayout.CENTER);
         this.getContentPane().setBackground(new Color(176 , 0 , 9));
         this.setVisible(true);
+        loadingFrame.setVisible(false);
         System.out.println("Main Frame Added!");
     }
     public void paintComponent(Graphics g)
