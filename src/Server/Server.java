6347877ec86edf73865ac6c555e257252f50cf82
9731130 <ahmadrezadl@aut.ac.ir>
2019-06-26 18:06:10 +0430

    Elham: Adding Socket

---
+++ src/Server/Server.java
@@ -0,0 +1,34 @@
+package Server;
+
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Server {
+    ServerSocket mServer;
+    int serverPort = 9090;
+    ArrayList<Thread> threads  = new ArrayList<Thread>();
+    HashMap<String , ClientManager> clientsMap = new HashMap<String, ClientManager>();
+
+    public Server() throws IOException {
+        mServer = new ServerSocket(serverPort);
+        System.out.println("server created.");
+
+        while(true) {
+            Socket client = mServer.accept();
+            System.out.println("new Client added");
+            Thread t = new Thread(new ClientManager(this , client));
+
+            threads.add(t);
+
+            t.start();
+
+        }
+        public void addClientManager()
+
+    }
+
+}
