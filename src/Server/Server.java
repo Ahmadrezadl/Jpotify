b2bbc728020aecf95897f01e3adf5bb8e18a151f
elhamrazi <elhamrazi99@gmail.com>
2019-06-27 01:05:53 +0430

    Server and ClientManager classes added! ps: Server is complete but the other one needs work:)

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -6,10 +6,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
-/*    ServerSocket mServer;
-    int serverPort = 9090;
+   ServerSocket mServer;
+    int serverPort = 20019;
     ArrayList<Thread> threads  = new ArrayList<Thread>();
     HashMap<String , ClientManager> clientsMap = new HashMap<String, ClientManager>();
 
@@ -20,15 +21,35 @@ public class Server {
         while(true) {
             Socket client = mServer.accept();
             System.out.println("new Client added");
-           // Thread t = new Thread(new ClientManager(this , client));
+            Thread t = new Thread(new ClientManager(this , client));
 
             threads.add(t);
 
-          //  t.start();
+            t.start();
 
         }
         //public void addClientManager()
 
     }
-*/
+    public void addClientManager(String clientName, ClientManager cm) {
+        clientsMap.put(clientName, cm);
+    }
+
+    public ClientManager findClientManager(String clientName) {
+        return clientsMap.get(clientName);
+    }
+
+    public ArrayList<ClientManager> findAllClientManagers() {
+        ArrayList<ClientManager> result = new ArrayList<>();
+        for (Map.Entry<String, ClientManager> entry : clientsMap.entrySet()) {
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Server();
+    }
+
+
 }
