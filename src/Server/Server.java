dbaa0bca0ae97fbb33b99161d054bf91558201c8
elhamrazi <elhamrazi99@gmail.com>
2019-06-26 18:34:44 +0430

    Right menu added!

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Server {
-    ServerSocket mServer;
+/*    ServerSocket mServer;
     int serverPort = 9090;
     ArrayList<Thread> threads  = new ArrayList<Thread>();
     HashMap<String , ClientManager> clientsMap = new HashMap<String, ClientManager>();
@@ -20,15 +20,15 @@ public class Server {
         while(true) {
             Socket client = mServer.accept();
             System.out.println("new Client added");
-            Thread t = new Thread(new ClientManager(this , client));
+           // Thread t = new Thread(new ClientManager(this , client));
 
             threads.add(t);
 
-            t.start();
+          //  t.start();
 
         }
-        public void addClientManager()
+        //public void addClientManager()
 
     }
-
+*/
 }
