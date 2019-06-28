d0c6c8a1f0fbc1388dbeaafbb5c1b651ad75fca8
elhamrazi <elhamrazi99@gmail.com>
2019-06-29 01:45:49 +0430

    favoriteButton fixed! ps : kinda:)

---
+++ src/FavoriteButton.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FavoriteButton extends JButton implements ActionListener {
+    AppObjects appObjects;
+    boolean liked;
+
+    public FavoriteButton(AppObjects appObjects) {
+        this.appObjects = appObjects;
+        liked = false;
+        this.setText("like");
+        this.setFocusable(false);
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(!appObjects.getLastPlayed().heart){
+            appObjects.getFavorites().addSong(new SongButton(appObjects.getLastPlayed().link , appObjects.getLastPlayed().name , appObjects , appObjects.getFavorites()));
+            appObjects.getAllSongsPanel().refresh();
+            liked = true;
+            appObjects.getLastPlayed().heart = true;
+        }
+    }
+}
