4e3244f927b8636adabc12a590d66c871b78500f
elhamrazi <elhamrazi99@gmail.com>
2019-06-28 17:57:30 +0430

    Shuffle class added and shuffle is available now!

---
+++ src/ShuffleButton.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ShuffleButton extends JButton implements ActionListener {
+    AppObjects appObjects;
+    boolean pressed;
+    public ShuffleButton(AppObjects appObjects) {
+        super("shuffle");
+        this.setText("Fuck");
+        this.appObjects = appObjects;
+//        this.setBorderPainted(false);
+//        this.setContentAreaFilled(false);
+//        this.setBorder(null);
+        this.setBackground(Color.white);
+        this.setFocusable(false);
+        this.addActionListener(this);
+        pressed =false ;
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(pressed == false) {
+            appObjects.getBottomMenu().shuffle = true;
+            pressed = true;
+            this.setText("fucked");
+        }
+        else{
+            appObjects.getBottomMenu().shuffle = false;
+            pressed = false;
+            this.setText("fuck");
+
+        }
+
+    }
+}
